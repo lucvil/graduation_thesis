674c2a5e522356ed5a32f628869d93f86271d905
sail <sail@sailnoiMac.local>
2023-01-18 21:53:19 +0900

    before encrytest

--- db_manipulate.py
+++ db_manipulate.py
@@ -438,7 +438,7 @@ def encry_test():
 
 	encrypt_method = "add"
 	plain_json_folder = ["3.2", "5.2", "7.2", "9.2"]
-	plain_json_count = ["50", "100", "200", "500"]
+	plain_json_count = ["10", "20", "50", "100", "200"]
 	exp_count_config = 3
 	for exp_count in range(exp_count_config):
 		if exp_count == 0:
